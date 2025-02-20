cf2cc6e5951395702b21f2b35d87ff4187809368
Kiara Geuten <kiara.geuten@student.ehb.be>
2025-02-20 15:40:34 +0100

    warmup 2

--- main.js
+++ main.js
@@ -6,27 +6,56 @@ warmup1();
 function warmup1() {
   // naam veranderen
   console.log("Exercise week 1");
-  const button = document.querySelector("#button-1a");
-  button.addEventListener("click", function () {
+  document.querySelector("#button-1a").addEventListener("click", function () {
     document.querySelector("#content-1").innerHTML =
       '<p>Hallo, dit is de oefening van Kiara"</p>';
   });
-  document.querySelector("#button-1b");
-  addEventListener("click", function () {
+  document.querySelector("#button-1b").addEventListener("click", function () {
     document.querySelector("#section-1").style.backgroundColor = "lightblue";
   });
-  document.querySelector("#button-1c");
-  addEventListener("click", function () {
+  document.querySelector("#button-1c").addEventListener("click", function () {
     const Ready = document.createElement("p");
     Ready.id = "Ready";
-    Ready.innerHTML = "<strong><p>Ik ben er klaar voor</p>";
+    Ready.innerHTML = "<strong><p>Ik ben er klaar voor</strong></p>";
     document.querySelector("#content-1").append(Ready);
   });
 }
-
+warmup2();
 function warmup2() {
-  console.log("Exercise week 2");
   const scores = getRandomScores(10);
+  console.log("Exercise week 2");
+  document.querySelector("#button-2b").addEventListener("click", function () {
+    //TODO: sort
+    scores.sort(function (a, b) {
+      if (a > b) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+    let html = "<ul>";
+    //add li
+    scores.forEach(function (score) {
+      html += `<li>${score}</li>`;
+    });
+    html += "</ul>";
+    document.querySelector("#content-2").innerHTML = html;
+  });
+  document.querySelector("#button-2a").addEventListener("click", function () {
+    // add lijst
+    let html = "<ul>";
+    //add li
+    scores.forEach(function (score) {
+      html += `<li>${score}</li>`;
+    });
+    html += "</ul>";
+    document.querySelector("#content-2").innerHTML = html;
+  });
+  document.querySelector("#button-2c").addEventListener("click", function () {
+    const sum = scores.reduce(function (total, current) {
+      return total + current;
+    });
+  });
 }
 
 function warmup3() {
